900ee572917dce47da9ddeb6d560e65e15594b73
Ola Ishola <olaishola@hotmail.co.uk>
2022-05-23 11:26:44 +0100

    Add allowed params in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(sign_up) do |u|
+            u.permit(:name, :email, :password, :password_confirmation)
+        end
+
+        devise_parameter_sanitizer.permit(account_update) do |u|
+            u.permit(:name, :email, :password, :password_confirmation, :current_password)
+        end
+
+        devise_parameter_sanitizer.permit(:sign_in) do |u|
+            u.permit(:email, :password)
+          end
+    end
 end
